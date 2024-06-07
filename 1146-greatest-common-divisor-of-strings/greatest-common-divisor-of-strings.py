604ec42d26c31de83ff177f8e6f7db3ee1ebe6d0
sanketgadhave <148018483+sanketgadhave@users.noreply.github.com>
2024-06-07 19:25:53 -0400

    Time: 30 ms (90.21%) | Memory: 16.5 MB (91.90%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,12 +1,6 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        output_str = ''
-        for i in range(1, len(str1)+1):
-            sub_st = str1[0:i]
-            if len(sub_st)*str1.count(sub_st) == len(str1): 
-                if sub_st in str2:
-                    if len(sub_st)*str2.count(sub_st) == len(str2):
-                        if len(sub_st) > len(output_str):
-                            output_str = sub_st
-                
-        return output_str
+        if str1 + str2 != str2 + str1:
+            return ""
+        from math import gcd
+        return str1[:gcd(len(str1), len(str2))]
