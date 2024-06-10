61f376943491e6cdd16ab2deff405e9abaab0fef
sanketgadhave <148018483+sanketgadhave@users.noreply.github.com>
2024-06-09 22:18:08 -0400

    Time: 137 ms (26.57%) | Memory: 16.9 MB (62.99%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -1,22 +1,16 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        count = n
-        next_ele = -1
-        for i in range(0, len(flowerbed)):
-            if count > 0:
-                if i == 0:
-                    prev_ele = -1
-                else:
-                    prev_ele = flowerbed[i-1]
-                if i == len(flowerbed) - 1:
-                    next_ele = -1
-                else:
-                    next_ele = flowerbed[i+1]
-                if (prev_ele == 0 or prev_ele == -1) and flowerbed[i] == 0 and (next_ele == 0 or next_ele == -1):
-                    flowerbed[i] = 1
-                    count -= 1
+        count = 0
+        for i in range(len(flowerbed)):
+            # Check if the current plot is empty.
+            if flowerbed[i] == 0:
+                # Check if the left and right plots are empty.
+                empty_left_plot = (i == 0) or (flowerbed[i - 1] == 0)
+                empty_right_lot = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
                 
-        if count == 0:
-            return True
-        else:
-            return False
+                # If both plots are empty, we can plant a flower here.
+                if empty_left_plot and empty_right_lot:
+                    flowerbed[i] = 1
+                    count += 1
+                    
+        return count >= n
