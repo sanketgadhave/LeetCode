46a74e37aec5cffeea477209ec2ee9a33c760ba1
sanketgadhave <148018483+sanketgadhave@users.noreply.github.com>
2024-11-09 18:53:35 -0500

    Time: 0 ms (100.00%) | Memory: 16.7 MB (32.51%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,8 +1,12 @@
 from math import gcd
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if str1 + str2 != str2 + str1:
-            return ""
-        else: 
-            return str1[:gcd(len(str1),len(str2))]
+        min_len = min(len(str1), len(str2))
+        for i in range(min_len, 0, -1):
+            if len(str1) % i == 0 and len(str2) % i == 0:
+                candidate = str1[:i]
+                if candidate * (len(str1)//i) == str1 and candidate * (len(str2)//i) == str2:
+                    return candidate
+                
+        return ''
 
