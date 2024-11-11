61e758be5f13605ec358c7bde753a2096aed6612
sanketgadhave <148018483+sanketgadhave@users.noreply.github.com>
2024-11-11 13:09:58 -0500

    Time: 26 ms (18.39%) | Memory: 18.7 MB (14.74%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -1,21 +1,24 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        char_list = list(s)
+        vowel_list = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
         start = 0
-        end = len(char_list) - 1
-        vowel_list = ['a','e','i','o','u','A','E','I','O','U']
-        while start < end:
-            if char_list[start] in vowel_list:
-                if char_list[end] in vowel_list:
-                    temp = char_list[start]
-                    char_list[start] = char_list[end]
-                    char_list[end] = temp
+        end = len(s)-1
+        s_list = list(s)
+        while start<end:
+            if s_list[start] in vowel_list or s_list[end] in vowel_list:
+                if s_list[start] in vowel_list and s_list[end] in vowel_list:
+                    s_list[start], s_list[end] = s_list[end], s_list[start]
                     start += 1
-                    end -= 1
-                    continue
-                else:
-                    end -= 1
-                    continue
+                    end -=1
+                elif s_list[start] in vowel_list:
+                    end -=1
+                elif s_list[end] in vowel_list:
+                    start +=1
+            else:
+                start +=1
+                end -=1
+        
+        output = ''.join(char for char in s_list)
+        return output
 
-            start += 1
-        return ''.join(char_list)
+                
