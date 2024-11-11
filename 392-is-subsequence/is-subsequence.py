b626ce80084505dd0f87a9b57fd75cb94139eded
sanketgadhave <148018483+sanketgadhave@users.noreply.github.com>
2024-11-11 15:43:35 -0500

    Time: 0 ms (100.00%) | Memory: 16.6 MB (40.80%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,17 +1,15 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        s_list = list(s)
-        t_list = list(t)
         flag = -1
         count = 0
-        for i in range(len(s_list)):
-            for j in range(flag+1, len(t_list)):
-                if s_list[i] == t_list[j]:
+        for i in range(len(s)):
+            for j in range(flag+1, len(t)):
+                if s[i] == t[j]:
                     flag = j
-                    count+=1
+                    count +=1
                     break
         
-        if count == len(s_list):
+        if count == len(s):
             return True
         else:
             return False
