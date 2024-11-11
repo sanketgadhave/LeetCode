e83a07b1772c15dc1015d8bb3bbc4461fa926914
sanketgadhave <148018483+sanketgadhave@users.noreply.github.com>
2024-11-11 15:36:45 -0500

    Time: 4 ms (64.29%) | Memory: 17.7 MB (94.60%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,7 +3,9 @@ class Solution:
         left = 0
         for right in range(len(nums)):
             if nums[right] != 0:
-                nums[right], nums[left] = nums[left], nums[right]
-                left += 1
+                nums[left], nums[right] = nums[right], nums[left]
+                left +=1
+        
+        
             
         
